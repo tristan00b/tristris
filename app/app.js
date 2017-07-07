c0d7011940fe3005c38263e24fdade37f7601101
Tristan Bayfield <tristan.bayfield@sfu.ca>
2017-07-06 22:32:31 -0700

    Enable HTML hot reloading during development

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
-import './app.sass'
 import 'babel-polyfill'
+import './app.sass'
 import Tetris from './src/tetris.js'
 import config from './assets/data/config.json'
+
+if (process.env.NODE_ENV !== 'production') {
+  // enable hot reloading during development
+  require('./index.html')
+}
 
 const canvas = {
   'main': document.getElementsByClassName('main-canvas')[0],
--- webpack.config.js
+++ webpack.config.js
@@ -4,72 +4,78 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = (env) => {
+const buildingForProduction =
+  !!(process.env.NODE_ENV && process.env.NODE_ENV === 'production');
 
-  let buildingForProduction = env && env.production;
-  let cssConfig = buildingForProduction
-    ? ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'sass-loader'],
-        publicPath: '/dist'
-      })
-    : ['style-loader', 'css-loader', 'sass-loader'];
+const cssConfig = buildingForProduction
+  ? ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: ['css-loader', 'sass-loader'],
+      publicPath: '/dist'
+    })
+  : ['style-loader', 'css-loader', 'sass-loader'];
 
-  return {
-    context: path.join(__dirname, 'app'),
-    devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      hot: true,
-      inline: true,
-    },
-    entry: {
-      app: ['babel-polyfill', './app.js']
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      pathinfo: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.sass$/,
-          use: cssConfig
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: 'babel-loader',
-        },
-        {
-          test: /\.json$/,
-          use: 'json-loader'
-        },
-        {
-          test: /\.tff$/,
-          use: 'url-loader'
-        },
-      ]
-    },
-    plugins: [
-      new CopyWebpackPlugin(
-        [ { from: 'assets', to: 'assets' } ],
-        { copyUnmodified: false, ignore: ['*.zip', '*.tff'] }
-      ),
-      new HtmlWebpackPlugin({
-        title: 'TrisTris',
-        template: 'index.ejs',
-        filename: 'index.html',
-        minify: { collapseWhitespace: buildingForProduction },
-        hash: true,
-      }),
-      new ExtractTextPlugin({
-        filename: 'app.css',
-        disable: !buildingForProduction,
-        allChunks: true
-      }),
-      new webpack.HotModuleReplacementPlugin(),
+module.exports = {
+  context: path.join(__dirname, 'app'),
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    inline: true
+  },
+  entry: {
+    app: ['babel-polyfill', './app.js']
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    pathinfo: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "raw-loader"
+      },
+      {
+        test: /\.sass$/,
+        use: cssConfig
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.tff$/,
+        use: 'url-loader'
+      },
     ]
-  };
+  },
+  plugins: [
+    new CopyWebpackPlugin(
+      [ { from: 'assets', to: 'assets' } ],
+      { copyUnmodified: true, ignore: ['*.tff', '*.zip', '.DS_Store'] }
+    ),
+    new HtmlWebpackPlugin({
+      title: 'TrisTris',
+      template: './index.html',
+      filename: 'index.html',
+      minify: { collapseWhitespace: buildingForProduction },
+      hash: true,
+    }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disable: !buildingForProduction,
+      allChunks: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+    	'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ]
 };
